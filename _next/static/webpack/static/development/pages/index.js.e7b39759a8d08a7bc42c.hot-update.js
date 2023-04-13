15c3a450d0a766cdd4353d26f50ed81f660d463e
aliang2022 <501747372@qq.com>
2023-04-13 15:15:36 +0800

    Updates

---
+++ _next/static/webpack/static/development/pages/index.js.e7b39759a8d08a7bc42c.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./src/store/mock/jobsInfo.json":
+/*!**************************************!*\
+  !*** ./src/store/mock/jobsInfo.json ***!
+  \**************************************/
+/*! exports provided: 0, 1, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("[{\"enterprise\":\"紫光软件系统有限公司\",\"links\":\"http://www.thunis.com/\",\"duties\":\"WEB前端工程师\",\"time\":\"2020-10\",\"logoName\":\"zgrj\",\"details\":\"1. 基于React+TypeScript+Antd+Dva构建项目，开发督查、任务管理信息系统，适配各终端应用。<br>2. 负责督查核心检查流程各环节页面、任务管理主流程相关功能的开发。<br>3. 推动在项目中进行模块化开发，降低业务代码复杂度，提高开发效率。解决实际业务场景问题和日常Bug。<br>4. 分析业务需求，同团队相互沟通，合作开发，同时给产品交互体验提出合适的建议。<br>5. 完成项目从零搭建到顺利上线并使用，及时处理客户提出的功能需求和问题。\",\"project\":\"工作流管理系统、任务管理信息系统、督查管理信息系统、资产使用周期管理系统。\"},{\"enterprise\":\"沈阳维创高科科技有限公司\",\"duties\":\"前端开发工程师\",\"links\":\"http://zhyl.wcgksoft.top/\",\"time\":\"2018-06\",\"logoName\":\"wcgk\",\"details\":\"1. 负责老司机出行新版官网开发，采用Vue+Nuxt+ElementUi SSR直出模式，有利于解决单页SPA SEO缺陷的问题。<br>2. 公司OA系统前端整站开发，通过钉钉APP端扫码登入企业办公系统，员工操作不同权限展示，上传文档、图片至阿里云oss存储，增强安全性，提升网站响应速度，添加前台编辑器功能发布消息同步到钉钉。<br>3. 同设计师，后端合作，高质量的完成页面的实现工作，利用前沿技术提高编码效率，缩短项目的开发周期。<br>4. 搭建技术框架，使用Git进行版本控制和迭代，前后分离架构，组件化来提高复用和可维护性。\",\"project\":\"智慧园林管控系统、智慧热电中控平台、智慧养殖大数据平台、智慧能源管控系统。\"}]");
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.e7b39759a8d08a7bc42c.hot-update.js.map
